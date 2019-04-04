a03e1e9c54085a59aff69ff2af91d46839fa485a
OzzieP7 <legendre.brendan@hotmail.fr>
2019-04-04 09:19:46 +0200

    .

---
+++ Dyslexique/Classes/Mot.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dyslexique.Classes
+{
+    public class Mot // Texte, Genre, Classe
+    {
+        #region Cours
+        // Attributs
+        private String texte;
+        private String type;
+
+        // Accesseurs de l'attribut Texte
+        public String getTexte()
+        {
+            return this.texte;
+        }
+        public void setTexte(String texte)
+        {
+            this.texte = texte;
+        }
+
+        public String getType()
+        {
+            return this.type;
+        }
+        public void setType(String type)
+        {
+            this.type = type;
+        }
+
+        // Constructeurs
+        public Mot()
+        {
+
+        }
+
+        public Mot(String texte)
+        {
+            this.texte = texte;
+        }
+
+        public Mot(String texte, String type)
+        {
+            this.texte = texte;
+            this.type = type;
+        }
+        #endregion
+
+        #region Nouveau
+        private int idMot;
+        public int IdMot
+        {
+            get { return idMot; }
+            set { idMot = value; }
+        }
+
+        private string text;
+        public string Texte
+        {
+            get { return text; }
+            set { text = value; }
+        }
+
+        private int idClasse;
+        public int IdClasse
+        {
+            get { return idClasse; }
+            set { idClasse = value; }
+        }
+
+        private int idGenre;
+        public int IdGenre
+        {
+            get { return idGenre; }
+            set { idGenre = value; }
+        }
+        #endregion
+
+    }
+}
---
+++ Dyslexique/Classes/Utilisateur.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dyslexique.Classes
+{
+    public class Utilisateur
+    {
+        private int idUtilisateur;
+        public int IdUtilisateur
+        {
+            get { return idUtilisateur; }
+            set { idUtilisateur = value; }
+        }
+
+        private string pseudo;
+        public string Pseudo
+        {
+            get { return pseudo; }
+            set { pseudo = value; }
+        }
+
+        private int idRole;
+        public int IdRole
+        {
+            get { return idRole; }
+            set { idRole = value; }
+        }
+    }
+}
